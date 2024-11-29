995d8c084b7cfde9800adac3e7881209d6661d76
deshaspoorthy <sprthyd@gmail.com>
2024-11-29 19:10:47 +0530

    Added formatter

--- src/main/java/com/sprthy/App.java
+++ src/main/java/com/sprthy/App.java
@@ -8,8 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        int x =9;
-        System.out.println(x);
-        System.out.println( "Hello World!" );
+        double x = Math.random();
+        System.out.println( "Hello World! " +x );
     }
 }
