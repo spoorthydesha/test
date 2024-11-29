de19e46b2e7f1fb5ca631c5de6014993b7ab8d0a
deshaspoorthy <sprthyd@gmail.com>
2024-11-29 18:51:38 +0530

    Initial  commit to test

---
+++ src/main/java/com/sprthy/App.java
@@ -0,0 +1,15 @@
+package com.sprthy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int x =9;
+        System.out.println(x);
+        System.out.println( "Hello World!" );
+    }
+}
